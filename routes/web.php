ab08721f8823a5ed1563c5cbb927821321b1846d
MDRobiulHassan <robiul110256@gmail.com>
2025-08-06 01:09:33 +0600

    Resolves Merge Conflicts in web.php

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,46 @@ Route::get('/password/forgot', function () {
 Route::post('/password/forgot', function () {
     return redirect()->route('login.form');
 })->name('password.submit');
+
+Route::get('/search', function () {
+    return view('search');
+})->name('search');
+
+Route::get('/search-result', function () {
+    return view('search_result');
+})->name('search.result');
+
+Route::get('/booking-step1', function () {
+    return view('booking_step1');
+})->name('booking.step1');
+
+Route::get('/booking-step2', function () {
+    return view('booking_step2');
+})->name('booking.step2');
+
+Route::get('/booking-step3', function () {
+    return view('booking_step3');
+})->name('booking.step3');
+
+
+Route::get('/booking-confirm', function () {
+    return view('booking_confirm');
+})->name('booking.confirm');
+
+
+Route::get('/userdashboard', function () {
+    return view('userdashboard');
+});
+
+Route::get('/userprofile', function () {
+    return view('userprofile');
+});
+
+Route::get('/schedule', function () {
+    return view('schedule');
+})->name('schedule');
+
+
+Route::get('/train-search', function () {
+    return view('train_search');
+})->name('train.search');
